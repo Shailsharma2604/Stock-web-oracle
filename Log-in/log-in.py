ed71ccf562d6bf5bde427703d1eb9a6cbdebea83
Shailsharma2604 <shailsharma020604@gmail.com>
2024-05-13 21:32:40 +0530

    1

--- Log-in/log-in.py
+++ Log-in/log-in.py
@@ -1,31 +1,41 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-db=SQLAlchemy()
-db.init_app(app)
+# db=SQLAlchemy()
+# db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/stock_web_oracle"
-
-
-class sign_in(db.Model):
-    Email = db.Column(db.String(255), unique = True, nullable = False),
-    Password = db.Column(db.String(255), nullable = False),
-    Sno = db.Column(db.Integer, primary_key = True)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/stock_web_oracle"
 
 
-class create_account(db.Model):
-    Sno = db.Column(db.Integer, primary_key = True),
-    Name = db.Column(db.String(255), unique = True, nullable = False),
-    Email = db.Column(db.String(255), unique = True, nullable = False),
-    Password = db.Column(db.String(255), nullable = False)
+# clss sign_in(db.Model):
+#     Email = db.Column(db.String(255), unique = True, nullable = False),
+#     Password = db.Column(db.String(255), nullable = False),
+#     Sno = db.Column(db.Integer, primary_key = True)
+
+
+# class create_account(db.Model):
+#     Sno = db.Column(db.Integer, primary_key = True),
+#     Name = db.Column(db.String(255), unique = True, nullable = False),
+#     Email = db.Column(db.String(255), unique = True, nullable = False),
+#     Password = db.Column(db.String(255), nullable = False)a
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('about.html')@app.route('/about')
+def index():
+    return render_template('index.html')@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Log-in/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+ss sign_in(db.Model):
+#     Email = db.Column(db.String(255), unique = True, nullable = False),
+#     Password = db.Column(db.String(255), nullable = False),
+#     Sno = db.Column(db.Integer, primary_key = True)
+
+
+# class create_account(db.Model):
+#     Sno = db.Column(db.Integer, primary_key = True),
+#     Name = db.Column(db.String(255), unique = True, nullable = False),
+#     Email = db.Column(db.String(255), unique = True, nullable = False),
+#     Password = db.Column(db.String(255), nullable = False)
